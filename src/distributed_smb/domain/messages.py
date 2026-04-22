60ceac05463f789c90978bc73954c3698b272cd0
filippo badioli <filippo.badioli@studio.unibo.it>
2026-04-22 15:35:15 +0200

    feat: added messages for sessions, roster and gamestart

--- src/distributed_smb/domain/messages.py
+++ src/distributed_smb/domain/messages.py
@@ -12,6 +12,11 @@ class MessageType(StrEnum):
 
     PLAYER_INPUT = "player_input"
     WORLD_STATE = "world_state"
+    SESSION_CREATE = "session_create"
+    SESSION_JOIN = "session_join"
+    ROSTER_UPDATE = "roster_update"
+    GAME_START = "game_start"
+    INITIAL_STATE_SYNC = "initial_state_sync"
 
 
 @dataclass(slots=True)
@@ -31,3 +36,33 @@ class WorldStateSnapshot:
     sequence_number: int
     world_state: WorldState = field(default_factory=WorldState)
     message_type: MessageType = field(init=False, default=MessageType.WORLD_STATE)
+
+@dataclass(slots=True)
+class SessionCreate:
+    player_id: str
+    message_type: MessageType = field(init=False, default=MessageType.SESSION_CREATE)
+
+@dataclass(slots=True)
+class SessionJoin:
+    session_id: str
+    player_id: str
+    ip: str
+    port: int
+    message_type: MessageType = field(init=False, default=MessageType.SESSION_JOIN)
+
+from distributed_smb.shared.roster import GlobalRoster
+
+@dataclass(slots=True)
+class RosterUpdate:
+    roster: GlobalRoster
+    message_type: MessageType = field(init=False, default=MessageType.ROSTER_UPDATE)
+
+@dataclass(slots=True)
+class GameStart:
+    session_id: str
+    message_type: MessageType = field(init=False, default=MessageType.GAME_START)
+
+@dataclass(slots=True)
+class InitialStateSync:
+    world_state: WorldState
+    message_type: MessageType = field(init=False, default=MessageType.INITIAL_STATE_SYNC)
